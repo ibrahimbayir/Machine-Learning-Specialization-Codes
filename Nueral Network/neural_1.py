c2c32cffe2d21f413a2eaf825a9f792d82b5b60b
ibrahim bayır <ibrahim54gfb@outlook.com>
2024-08-08 16:39:18 +0300

    Bu kod, veri kümesindeki özellikleri normalleştirerek, her bir özelliği sıfır ortalamalı ve birim varyanslı bir dağılıma dönüştürmeyi amaçlar.

--- Nueral Network/neural_1.py
+++ Nueral Network/neural_1.py
@@ -15,3 +15,11 @@ X,Y = load_coffee_data();
 print(X.shape, Y.shape)
 
 plt_roast(X,Y)
+
+print(f"Temperature Max, Min pre normalization: {np.max(X[:,0]):0.2f}, {np.min(X[:,0]):0.2f}")
+print(f"Duration    Max, Min pre normalization: {np.max(X[:,1]):0.2f}, {np.min(X[:,1]):0.2f}")
+norm_l = tf.keras.layers.Normalization(axis=-1)
+norm_l.adapt(X)  # learns mean, variance
+Xn = norm_l(X)
+print(f"Temperature Max, Min post normalization: {np.max(Xn[:,0]):0.2f}, {np.min(Xn[:,0]):0.2f}")
+print(f"Duration    Max, Min post normalization: {np.max(Xn[:,1]):0.2f}, {np.min(Xn[:,1]):0.2f}")
