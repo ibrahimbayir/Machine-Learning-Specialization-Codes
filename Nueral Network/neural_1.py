e49f5a9713fd370c4124bef97f9347d981a5aecf
ibrahim bayır <ibrahim54gfb@outlook.com>
2024-08-08 16:35:21 +0300

    Bu kod, kahve verisetini yükler ve yüklenen veriyi iki boyutlu bir grafik olarak görselleştirir. Grafik, verilerin nasıl dağıldığını ve sınıfların birbirinden nasıl ayrıldığını gösterir.

---
+++ Nueral Network/neural_1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import ipywidgets
+import matplotlib.pyplot as plt
+plt.style.use('./deeplearning.mplstyle')
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from lab_utils_common import dlc
+from lab_coffee_utils import load_coffee_data, plt_roast, plt_prob, plt_layer, plt_network, plt_output_unit
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+tf.autograph.set_verbosity(0)
+
+X,Y = load_coffee_data();
+print(X.shape, Y.shape)
+
+plt_roast(X,Y)
