ae681969cf8841afc61d850f5aca9ed0fdbd24a7
ibrahim bayır <ibrahim54gfb@outlook.com>
2024-08-11 15:38:03 +0300

    Bu kodun tamamında veri yükleme ve görselleştirme, veri normalizasyonu, yoğun katman (dense layer) hesaplama, sinir ağı modeli tanımlama, tahmin fonksiyonu , sınıflandırma kararları ve ağın görselleştirilmesini görüyoruz.

--- Nueral Network/Neural 2/nn2.py
+++ Nueral Network/Neural 2/nn2.py
@@ -45,4 +45,36 @@ def my_sequential(x, W1, b1, W2, b2):
     a2 = my_dense(a1, W2, b2, sigmoid)
     return(a2)
 
+W1_tmp = np.array( [[-8.93,  0.29, 12.9 ], [-0.1,  -7.32, 10.81]] )
+b1_tmp = np.array( [-9.82, -9.28,  0.96] )
+W2_tmp = np.array( [[-31.18], [-27.59], [-32.56]] )
+b2_tmp = np.array( [15.41] )
+
+def my_predict(X, W1, b1, W2, b2):
+    m = X.shape[0]
+    p = np.zeros((m,1))
+    for i in range(m):
+        p[i,0] = my_sequential(X[i], W1, b1, W2, b2)
+    return(p)
+
+X_tst = np.array([
+    [200,13.9],  # postive example
+    [200,17]])   # negative example
+X_tstn = norm_l(X_tst)  # remember to normalize
+predictions = my_predict(X_tstn, W1_tmp, b1_tmp, W2_tmp, b2_tmp)
+
+yhat = np.zeros_like(predictions)
+for i in range(len(predictions)):
+    if predictions[i] >= 0.5:
+        yhat[i] = 1
+    else:
+        yhat[i] = 0
+print(f"decisions = \n{yhat}")
+
+yhat = (predictions >= 0.5).astype(int)
+print(f"decisions = \n{yhat}")
+
+netf= lambda x : my_predict(norm_l(x),W1_tmp, b1_tmp, W2_tmp, b2_tmp)
+plt_network(X,Y,netf)
+
 
