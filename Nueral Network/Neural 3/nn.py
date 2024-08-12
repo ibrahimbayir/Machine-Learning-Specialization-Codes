36078cc87757b53a4e8ffd891558cb716927236b
ibrahim bayır <ibrahim54gfb@outlook.com>
2024-08-12 07:44:48 +0300

    Bu kod ile çoklu sınıfların nasıl oluşturulduğunu, nasıl görselleştirildiğini, softmax fonksiyonu kullanımını ve tüm bunları yaparken çeşitli fonksiyonları kullanmayı öğrendik.

--- Nueral Network/Neural 3/nn.py
+++ Nueral Network/Neural 3/nn.py
@@ -17,8 +17,9 @@ m = 100
 centers = [[-5, 2], [-2, -2], [1, 2], [5, -2]]
 std = 1.0
 X_train, y_train = make_blobs(n_samples=m, centers=centers, cluster_std=std,random_state=30)
+##make_blobs funcition is useful to create an training dataset
 
-plt_mc(X_train,y_train,classes, centers, std=std)
+plt_mc(X_train,y_train,classes, centers, std=std)##visualize the dataset
 
 # show classes in data set
 print(f"unique classes {np.unique(y_train)}")
