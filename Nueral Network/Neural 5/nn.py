28a39757509c238db9fb5df0bd35fe7b4bc09e66
ibrahim bayır <ibrahim54gfb@outlook.com>
2024-08-16 15:57:20 +0300

    In addition, we see preferred and undpreferred models as neural networks and we have both models. The significant detail is Using from_logits=True in the "Preferred" model is considered a better practice because this method provides more stable and reliable results.

--- Nueral Network/Neural 5/nn.py
+++ Nueral Network/Neural 5/nn.py
@@ -25,7 +25,7 @@ plt_softmax(my_softmax)
 centers = [[-5, 2], [-2, -2], [1, 2], [5, -2]]
 X_train, y_train = make_blobs(n_samples=2000, centers=centers, cluster_std=1.0,random_state=30)
 
-model = Sequential(
+model = Sequential(##its called the obvious organization
     [
         Dense(25, activation = 'relu'),
         Dense(15, activation = 'relu'),
@@ -41,3 +41,35 @@ model.fit(
     X_train,y_train,
     epochs=10
 )
+
+p_nonpreferred = model.predict(X_train)
+print(p_nonpreferred [:2])
+print("largest value", np.max(p_nonpreferred), "smallest value", np.min(p_nonpreferred))
+
+preferred_model = Sequential( #its called preffered
+    [
+        Dense(25, activation='relu'),
+        Dense(15, activation='relu'),
+        Dense(4, activation='linear')  # <-- Note
+    ]
+)
+preferred_model.compile(
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),  # <-- Note it's a convention
+    optimizer=tf.keras.optimizers.Adam(0.001),
+)
+
+preferred_model.fit(
+    X_train, y_train,
+    epochs=10
+)
+
+p_preferred = preferred_model.predict(X_train)
+print(f"two example output vectors:\n {p_preferred[:2]}")
+print("largest value", np.max(p_preferred), "smallest value", np.min(p_preferred))
+
+sm_preferred = tf.nn.softmax(p_preferred).numpy()
+print(f"two example output vectors:\n {sm_preferred[:2]}")
+print("largest value", np.max(sm_preferred), "smallest value", np.min(sm_preferred))
+
+for i in range(5):
+    print( f"{p_preferred[i]}, category: {np.argmax(p_preferred[i])}")
